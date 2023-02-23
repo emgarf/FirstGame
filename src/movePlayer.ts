3583399c51580a3efa87e69306bde2e9936f6349
emgarf <blozz@hotmail.com>
2023-02-23 14:44:44 +0100

    smooth diagonal movement

--- src/movePlayer.ts
+++ src/movePlayer.ts
@@ -1,18 +1,20 @@
 export const movePlayer = (arrow, player) => {
+  const velocity = 350;
+  player.body.setMaxSpeed(velocity);
+
   if (arrow.left.isDown) {
-    player.body.setVelocityX(-350);
-    player.body.setVelocityY(0);
+    player.body.setVelocityX(-velocity);
   } else if (arrow.right.isDown) {
-    player.body.setVelocityX(350);
-    player.body.setVelocityY(0);
-  } else if (arrow.up.isDown) {
-    player.body.setVelocityY(-350);
-    player.body.setVelocityX(0);
-  } else if (arrow.down.isDown) {
-    player.body.setVelocityY(350);
-    player.body.setVelocityX(0);
+    player.body.setVelocityX(velocity);
   } else {
-      player.body.setVelocityX(0);
-      player.body.setVelocityY(0);
+    player.body.setVelocityX(0);
+  }
+  
+  if (arrow.up.isDown) {
+    player.body.setVelocityY(-velocity);
+  } else if (arrow.down.isDown) {
+    player.body.setVelocityY(velocity);
+  } else {
+    player.body.setVelocityY(0);
   }
 }
