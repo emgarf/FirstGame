6588bf39dc36b7c594e017b91eaf522251d01d37
emgarf <blozz@hotmail.com>
2023-02-23 12:21:24 +0100

    try fix sound

--- src/LoadScene.ts
+++ src/LoadScene.ts
@@ -7,7 +7,7 @@ export default class LoadScene extends Phaser.Scene {
     this.load.image('player', 'assets/player.png');
 		this.load.image('wallH', 'assets/wallHorizontal.png');
 		this.load.image('wallV', 'assets/wallVertical.png');
-		this.load.audio('pooboy', ['assets/pooboy.ogg', 'assets/pooboy.mp3']);
+		this.load.audio('pooboy', 'assets/pooboy.ogg');
 		this.load.image('enemy', 'assets/enemy.png');
 
     let loadLabel = this.add.text(250, 250, 'loading', {font: '30px Arial', color: '#fff'});
